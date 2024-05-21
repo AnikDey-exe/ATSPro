bb34b8b39889eec7236e06dca807b23eb6eef37c
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2024-05-21 11:35:58 -0400

    trying to debug slowness

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from ats_test3 import run_ats
+from atspro import ATSModel
 
 app = Flask(__name__)
 
@@ -9,6 +10,16 @@ CORS(app)
 @app.route("/")
 def entry_point():
     return "Base url"
+
+@app.route("/test", methods=["POST"])
+def test():
+    ats = ATSModel(model_path='atstweaked2.model')
+    dec = ats.is_similar("react", "business")
+    print(dec[1])
+    return {
+        "response": dec[0]
+    }
+
 
 @app.route("/ats", methods=["GET", "POST"])
 def ats():
